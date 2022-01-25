ebde3df1393e2796dcf5382be8a008526bfb456c
Sjarozha <hfy969@gmail.com>
2022-01-26 02:05:37 +0300

    modifying gamestatus

--- src/gamestate/gamestate.h
+++ src/gamestate/gamestate.h
@@ -2,21 +2,35 @@
 #ifndef GAMESTATE_H
 #define GAMESTATE_H
 
-#include <vector>
 #include "../player.h"
 #include "nlohmann/json.hpp"
+#include <unordered_map>
+#include <vector>
+
+struct WinPoints
+{
+    int capture;
+    int kill;
+};
+
 class GameState
 {
 public:
     GameState();
 
 public:
-
 private:
-    int                  num_players;
-    int                  num_turns;
-    int                  current_turn;
-    std::vector<Player> players;
+    int                                num_players;
+    int                                num_turns;
+    int                                current_turn;
+    std::vector<Player>                players;
+    std::vector<Player>                observers;
+    int                                current_player_idx;
+    bool                               finished;
+    std::vector<AbstractTank>          vehicles;
+    std::unordered_map<int, int>       attack_matrix;
+    int                                winner;
+    std::unordered_map<int, WinPoints> win_points;
 };
 
 void to_json(nlohmann::json& j, const GameState& gs);
