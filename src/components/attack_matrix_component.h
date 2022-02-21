1606b49990ce29c715c7943389ee5f931189bc66
Severin Klim <Severin.Klim@yandex.by>
2022-02-21 20:07:29 +0300

    Added AttackMatrixSystem

--- src/components/attack_matrix_component.h
+++ src/components/attack_matrix_component.h
@@ -16,6 +16,8 @@ public:
         attackMatrix[who].insert(whom);
     }
 
+    void ReplaceUserAttacks(uint64_t who, std::set<uint64_t> whom) { attackMatrix[who] = std::move(whom); }
+
     bool IsAttacked(uint64_t who, uint64_t whom)
     {
         if (attackMatrix.find(who) == attackMatrix.end())
@@ -27,9 +29,9 @@ public:
 
     void SetAttackMatrix(std::map<uint64_t, std::set<uint64_t>> matrix) { attackMatrix = std::move(matrix); };
 
+    void ClearUserAttacks(uint64_t who) { attackMatrix[who].clear(); }
+
     void ClearAttackMatrix() { attackMatrix.clear(); }
-
-
 
 private:
     std::map<uint64_t, std::set<uint64_t>> attackMatrix;
---
+++ src/systems/attack_matrix_system.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "ecs.h"
+#include "game/game_events.h"
+
+class AttackMatrixSystem : public ecs::System<AttackMatrixSystem>, public ecs::event::IEventListener
+{
+public:
+    AttackMatrixSystem();
+    ~AttackMatrixSystem() override;
+
+    void OnShootResponseEvent(const ShootResponseEvent* event);
+
+private:
+    void RegisterEventCallbacks();
+    void UnregisterEventCallbacks();
+
+};
